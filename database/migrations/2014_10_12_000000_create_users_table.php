4d5ecefd2a01605f83386fd5738f84fff0c385ed
Zibu <58338897+zibu9@users.noreply.github.com>
2023-11-29 08:08:56 +0100

    add roles and phone number to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique()->nullable();
+            $table->foreignId('role_id')->constrained()->default(4)->onUpdate('cascade');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
