b447f95a926d607dfd8fa2e7ea48e5a682ff8a10
Zibu <58338897+zibu9@users.noreply.github.com>
2023-11-29 10:56:26 +0100

    add relationship

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['role'];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
