431574e7ead2a97087359edfbea8b012dc632b36
Zibu <58338897+zibu9@users.noreply.github.com>
2023-11-30 13:09:17 +0100

    add template

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="" class="brand-link">
       <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">D&HRM</span>
+            style="opacity: .8">
+      <span class="brand-text font-weight-light">ObserVote</span>
     </a>
 
     <!-- Sidebar -->
@@ -31,7 +31,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{ route('formulaires.create') }}" class="nav-link">
+                <a href="" class="nav-link">
                   <i class="fa fa-plus nav-icon"></i>
                   <p>Nouveau
                     <span class="badge badge-success right">New</span>
@@ -39,7 +39,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{ route('formulaires.index') }}" class="nav-link">
+                <a href="" class="nav-link">
                   <i class="fas fa-list nav-icon"></i>
                   <p>Liste
                     <span class="badge badge-success right">All</span>
@@ -60,7 +60,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{ route('city.create') }}" class="nav-link">
+                <a href="" class="nav-link">
                   <i class="fa fa-plus nav-icon"></i>
                   <p>Nouveau
                     <span class="badge badge-success right">New</span>
@@ -68,7 +68,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="{{ route('city.index') }}" class="nav-link">
+                <a href="" class="nav-link">
                   <i class="fas fa-list nav-icon"></i>
                   <p>Liste
                     <span class="badge badge-success right">All</span>
