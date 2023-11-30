84a352aa3a9d2f828cc86071f49290581dd17d77
Zibu <58338897+zibu9@users.noreply.github.com>
2023-11-30 15:06:01 +0100

    update migratoon

--- database/migrations/2023_11_30_140151_add_cadidat_id_to_users_table.php
+++ database/migrations/2023_11_30_140151_add_cadidat_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->foreignId('candidat_id')->nullable();
         });
     }
 
