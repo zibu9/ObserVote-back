d2a8b52cbe432a2a037c15edeebffc5a79254291
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-01 21:32:16 +0100

    à zero

--- app/Http/Controllers/CandidatController.php
+++ app/Http/Controllers/CandidatController.php
@@ -1,1 +1,20 @@
- 
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class CandidatController extends Controller
+{
+    public function createObserver()
+    {
+        if (Gate::allows('admin-access')) {
+
+            return view('admin.observer');
+        } else {
+            abort(403);
+        }
+
+    }
+}
