eeb74707c2da0aa3e8f829358154da3b63bdcd65
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-02 01:56:20 +0100

    observer flow

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -28,6 +28,69 @@
             </div>
         @endif
 
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Mes Observateurs</h3>
+
+                    <div class="card-tools">
+                      <div class="input-group input-group-sm" style="width: 150px;">
+                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                        <div class="input-group-append">
+                          <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body table-responsive p-0" style="height: 400px;">
+                    <table class="table table-head-fixed text-nowrap">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Noms</th>
+                          <th>Candidat</th>
+                          <th>Status</th>
+                          <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @php
+                        $i = 1;
+                        @endphp
+                        @foreach ($observers as $observer)
+                        <tr>
+                          <td>{{ $observer->id }}</td>
+                          <td>{{ $observer->name }}</td>
+                          <td>{{ $observer->candidat->candidat }}</td>
+                          <td><span class="tag tag-success">Approved</span></td>
+                          <td class="">
+                                <a class="btn btn-info btn-sm" href="#">
+                                    <i class="fas fa-pencil-alt">
+                                    </i>
+                                    Edit
+                                </a>
+                                <a class="btn btn-danger btn-sm" href="#">
+                                    <i class="fas fa-trash">
+                                    </i>
+                                    Bloquer
+                                </a>
+                             </td>
+                        </tr>
+                        @php
+                            $i++;
+                        @endphp
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                  <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+              </div>
+        </div>
 
     </section>
 @endsection
