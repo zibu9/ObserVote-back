97a0e6b7cf1b7d537c296555c155966d26a23237
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-03 06:23:04 +0100

    update component

--- app/Livewire/SendResult.php
+++ app/Livewire/SendResult.php
@@ -6,8 +6,27 @@ use Livewire\Component;
 
 class SendResult extends Component
 {
+    public $step = 1;
+    public $centre = '';
+    public $centreCode = '';
+    public $bureau = '';
+    public $votantInitial = '';
+    public $votant = '';
+    public $nosVoix = '';
+
     public function render()
     {
         return view('livewire.send-result');
     }
+
+    public function nextStep()
+    {
+        $this->step++;
+    }
+
+    public function previousStep()
+    {
+        $this->step--;
+    }
+
 }
