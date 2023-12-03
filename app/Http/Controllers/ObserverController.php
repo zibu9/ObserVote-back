aefd76f7cb59ed31564acac7f79d7eff38acb3e4
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-03 05:35:55 +0100

    resultat flow

--- app/Http/Controllers/ObserverController.php
+++ app/Http/Controllers/ObserverController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ObserverController extends Controller
 {
-    //
+    public function createObserver()
+    {
+        if (Gate::allows('observer-access')) {
+
+            return view('observer.resultat');
+        } else {
+            abort(403);
+        }
+
+    }
 }
