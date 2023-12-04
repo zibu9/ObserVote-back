1fc34534423941462c836481e81846f46d2751d4
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-04 16:05:32 +0100

    validateStep

--- app/Livewire/SendResult.php
+++ app/Livewire/SendResult.php
@@ -14,6 +14,7 @@ class SendResult extends Component
     public $votant = '';
     public $nosVoix = '';
     public $bulletinRestant = '';
+    public $filled = [false, false, false, false, false, false, false];
 
     public function render()
     {
@@ -28,6 +29,37 @@ class SendResult extends Component
     public function previousStep()
     {
         $this->step--;
+    }
+
+    private function validateStep()
+    {
+        switch ($this->step) {
+            case 1:
+                $this->validate(['centre' => 'required']);
+                break;
+
+            case 2:
+                $this->validate(['centreCode' => 'required|email']);
+                break;
+
+            case 3:
+                $this->validate(['bureau' => 'required']);
+                break;
+            case 4:
+                $this->validate(['votantInitial' => 'required']);
+                break;
+            case 5:
+                $this->validate(['votant' => 'required']);
+                break;
+            case 6:
+                $this->validate(['nosVoix' => 'required']);
+                break;
+            case 7:
+                $this->validate(['bulletinRestant' => 'required']);
+                break;
+        }
+
+        $this->filled[$this->step - 1] = true;
     }
 
 }
