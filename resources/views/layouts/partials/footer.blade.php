fdaff2ddd5cc4da18017e7af5d3294bca78d098e
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-04 19:50:55 +0100

    add modal

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <strong> &copy; @php
         date('Y')
-    @endphp <a href="https://adminlte.io">AdminLTE.io</a></strong>
+    @endphp <a href="https://adminlte.io" target="_blank">AdminLTE.io</a></strong>
     All rights. Integrated by <a href="https://skytech243.com">Skytech243</a>
   </footer>
--- resources/views/observer/index.blade.php
+++ resources/views/observer/index.blade.php
@@ -78,11 +78,11 @@
                           <td>{{ $result->observer->candidat->candidat }}</td>
 
                           <td class="">
-                                <a class="btn btn-info btn-sm" href="#">
+                                <button class="btn btn-info btn-sm" data-toggle="modal" data-target="#modal-default">
                                     <i class="fas fa-pencil-alt">
                                     </i>
                                     Edit
-                                </a>
+                                </button>
                              </td>
                         </tr>
                         @php
@@ -97,6 +97,26 @@
                 <!-- /.card -->
               </div>
         </div>
-
+        <div class="modal fade" id="modal-default">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h4 class="modal-title">Default Modal</h4>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>One fine body&hellip;</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+              <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+          </div>
     </section>
 @endsection
