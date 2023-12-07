a5782d4969296fa8fbe5d0006e4c2abffcb3245f
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-07 10:34:50 +0100

    add livewire add candidat

---
+++ app/Livewire/AddCandidat.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class AddCandidat extends Component
+{
+    public function render()
+    {
+        return view('livewire.add-candidat');
+    }
+}
