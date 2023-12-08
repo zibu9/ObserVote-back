b1c55080d9913202ac042221a50949c0be5bde6e
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-08 13:42:26 +0100

    add css class

--- resources/views/livewire/send-result.blade.php
+++ resources/views/livewire/send-result.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="card-body">
-        <p for="name" class="text-danger">!! Assurez vous de bien selectionner la province et circonscription car c'est pas Modifiable.</p>
+        <p for="name" class="text-danger font-weight-bold">!! Assurez vous de bien selectionner la province et circonscription car c'est pas modifiable après.</p>
         <div class="form-group">
             <label for="name">Candidat</label>
             <input type="text" disabled class="form-control" value="{{ auth()->user()->candidat->candidat }}">
