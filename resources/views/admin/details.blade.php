1ab923a48b47245e9a8776c280106f9d4b0d5351
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-10 00:26:45 +0100

    fini la jr

--- resources/views/admin/details.blade.php
+++ resources/views/admin/details.blade.php
@@ -145,7 +145,12 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <canvas id="myChart" width="400" height="400"></canvas>
+                        <div class="row">
+                            <div class="col-md-2"></div>
+                            <div class="col-md-8">
+                                <canvas id="myChart"></canvas>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -165,12 +170,28 @@
 <!-- Étape 2: Préparer vos données -->
 <script>
     const data = {
-        labels: ['Label 1', 'Label 2', 'Label 3'],
+        labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5', 'Label 6', 'label 7'],
         datasets: [{
             label: 'My First Dataset',
-            data: [65, 59, 80],
-            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(255, 159, 64, 0.2)', 'rgba(255, 205, 86, 0.2)'],
-            borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)'],
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+            ],
             borderWidth: 1
         }]
     };
