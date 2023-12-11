e25daaa2fba108ce7080aa7f48b68495761810f7
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-11 11:06:42 +0100

    change password form

--- resources/views/admin/change-password.blade.php
+++ resources/views/admin/change-password.blade.php
@@ -1,5 +1,82 @@
 @extends('layouts.app')
+@section('title', 'change password')
 
-@section('content')
+@section('main')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Dashboard</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Acceuil</a></li>
+                    <li class="breadcrumb-item"><a href="">Admin</a></li>
+                    <li class="breadcrumb-item active">Password</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="content">
+                @if(session('success'))
+                    <div class="alert alert-success alert-dismissible">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                <!-- Le reste de votre contenu -->
+            </div>
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <div class="card card-primary">
+                    <div class="card-header">
+                      <h3 class="card-title">Modifier mot de Passe</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="ancien mot de passe" name="password" required minlength="4">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="nouveau mot de passe" name="new_password" required minlength="4">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                            </div>
 
+                            <div class="input-group mb-3">
+                            <input type="password" class="form-control" placeholder="repeter nouveau mot de passe" name="new_password2" required minlength="4">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="row">
+                            <div class="">
+                                <button type="submit" class="btn btn-primary btn-block">Modifier</button>
+                            </div>
+                            <!-- /.col -->
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-->
+        </div>
+        <!-- ./row -->
+    </section>
 @endsection
