60c688e44752c8dd5e24a7563646ef5d24637b6f
Zibu <58338897+zibu9@users.noreply.github.com>
2023-12-14 11:35:46 +0100

    add auth controllers for api endpoint

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $token = $request->user()->createToken('authToken')->plainTextToken;
+
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+}
