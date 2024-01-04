4d41f917fd649dac0f570ba55df462315c499038
Zibu <58338897+zibu9@users.noreply.github.com>
2024-01-04 10:17:39 +0100

    update

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -8,11 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class isAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         if (auth()->check() && auth()->user()->role->role === 'Admin')
